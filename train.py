4951e5e34f187d5bd120614b7ca5a21f61e0f29c
rhcsky <dev.rhcsky@gmail.com>
2021-02-01 11:35:18 +0900

    Update train.py

--- train.py
+++ train.py
@@ -16,11 +16,12 @@ from utils import AverageMeter
 
 best_acc1 = 0
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+args = get_config()
+writer = SummaryWriter(args.log_dir)
 
 
 def main():
     global args, best_acc1, device
-    args = get_config()
 
     # Init seed
     torch.cuda.cudnn_enabled = False
@@ -43,7 +44,6 @@ def main():
 
     optimizer = torch.optim.Adam(model.parameters(), args.lr)
 
-    writer = SummaryWriter(args.log_dir)
     cudnn.benchmark = True
 
     if args.resume:
@@ -69,8 +69,8 @@ def main():
 
     for epoch in range(start_epoch, args.epochs):
 
-        train_loss = train(train_loader, model, optimizer, criterion)
-        val_loss, acc1 = validate(val_loader, model, criterion)
+        train_loss = train(train_loader, model, optimizer, criterion, epoch)
+        val_loss, acc1 = validate(val_loader, model, criterion, epoch)
 
         if acc1 >= best_acc1:
             is_best = True
@@ -85,9 +85,8 @@ def main():
             'optimizer_state_dict': optimizer.state_dict(),
         }, is_best)
 
-        writer.add_scalar("Loss/Train", train_loss, epoch)
-        writer.add_scalar("Loss/Val", val_loss, epoch)
-        writer.add_scalar("Acc/Top1", acc1, epoch)
+        writer.add_scalar("Loss/1Epoch", val_loss, epoch)
+        writer.add_scalar("Acc/1Epoch", acc1, epoch)
 
         print(f"[{epoch}/{args.epochs}] {train_loss:.3f}, {val_loss:.3f}, {acc1:.3f}, # {best_acc1:.3f}")
 
@@ -96,9 +95,10 @@ def main():
     writer.close()
 
 
-def train(train_loader, model, optimizer, criterion):
+def train(train_loader, model, optimizer, criterion, epoch):
     losses = AverageMeter()
     num_support = args.num_support_tr
+    total_epoch = len(train_loader) * epoch
 
     # switch to train mode
     model.train()
@@ -106,7 +106,7 @@ def train(train_loader, model, optimizer, criterion):
         input, target = data[0].to(device), data[1].to(device)
 
         output = model(input)
-        loss, _ = criterion(output, target, num_support)
+        loss, acc1 = criterion(output, target, num_support)
 
         losses.update(loss.item(), input.size(0))
 
@@ -115,25 +115,32 @@ def train(train_loader, model, optimizer, criterion):
         loss.backward()
         optimizer.step()
 
+        writer.add_scalar("Loss/Train", loss.item(), total_epoch + i)
+        writer.add_scalar("Acc/Train", acc1.item(), total_epoch + i)
+
     return losses.avg
 
 
-def validate(val_loader, model, criterion):
+@torch.no_grad()
+def validate(val_loader, model, criterion, epoch):
     losses = AverageMeter()
     top1 = AverageMeter()
     num_support = args.num_support_val
+    total_epoch = len(val_loader) * epoch
 
     # switch to evaluate mode
     model.eval()
-    with torch.no_grad():
-        for i, data in enumerate(val_loader):
-            input, target = data[0].to(device), data[1].to(device)
+    for i, data in enumerate(val_loader):
+        input, target = data[0].to(device), data[1].to(device)
 
-            output = model(input)
-            loss, acc1 = criterion(output, target, num_support)
+        output = model(input)
+        loss, acc1 = criterion(output, target, num_support)
 
-            losses.update(loss.item(), input.size(0))
-            top1.update(acc1.item(), input.size(0))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1.item(), input.size(0))
+
+        writer.add_scalar("Loss/Val", loss.item(), total_epoch + i)
+        writer.add_scalar("Acc/Val", acc1.item(), total_epoch + i)
 
     return losses.avg, top1.avg
 
