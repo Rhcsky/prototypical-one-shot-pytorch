bd7e198c609c74c77d7e5f02fc45514a2ce70bb8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 06:49:06 +0000

    Adding tarfile member sanitization to extractall()

--- dataloader.py
+++ dataloader.py
@@ -101,7 +101,26 @@ class MiniImagenetDataset(Dataset):
         filename = os.path.join(root, gz_filename)
 
         with tarfile.open(filename, 'r') as f:
-            f.extractall(root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, root)
 
         os.rename('data/miniImagenet/mini-imagenet-cache-train.pkl', 'data/miniImagenet/train')
         os.rename('data/miniImagenet/mini-imagenet-cache-val.pkl', 'data/miniImagenet/val')
